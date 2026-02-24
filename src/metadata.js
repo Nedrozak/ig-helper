7e892428c241dc8cea919295adf61ec4248d6f69
5026 <40320172+SN-Koarashi@users.noreply.github.com>
2026-02-25 00:43:09 +0800

    Update the SRI for mediabunny dependencies.

--- src/metadata.js
+++ src/metadata.js
@@ -26,8 +26,8 @@
 // @grant              GM_openInTab
 // @connect            i.instagram.com
 // @connect            raw.githubusercontent.com
+// @require            https://cdn.jsdelivr.net/npm/mediabunny@1.34.5/dist/bundles/mediabunny.min.cjs#sha256-wUFR+x2bDvpqgMAVGy2CvGvULyjTGvGy4UUAm8rae5U=
 // @require            https://code.jquery.com/jquery-3.7.1.min.js#sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=
-// @require            https://cdn.jsdelivr.net/npm/mediabunny@1.34.5/dist/bundles/mediabunny.min.cjs
 // @resource           INTERNAL_CSS https://raw.githubusercontent.com/SN-Koarashi/ig-helper/master/style.css
 // @resource           LOCALE_MANIFEST https://raw.githubusercontent.com/SN-Koarashi/ig-helper/master/locale/manifest.json
 // @supportURL         https://github.com/SN-Koarashi/ig-helper/
